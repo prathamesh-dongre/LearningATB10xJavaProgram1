f476335904659452991f62635369bd94e2efefea
Prathamesh <prathameshdongre0@gmail.com>
2025-01-11 15:01:17 +0530

    New change1

---
+++ src/Tableof9.java
@@ -0,0 +1,16 @@
+public class Tableof9 {
+    public static void main(String [] args){
+        int a=9;
+        System.out.println("Table of 9");
+        System.out.printf("9x1=%d",a);
+        System.out.printf("%n9x2=%d",a*2);
+        System.out.printf("%n9x3=%d",a*3);
+        System.out.printf("%n9x4=%d",a*4);
+        System.out.printf("%n9x5=%d",a*5);
+        System.out.printf("%n9x6=%d",a*6);
+        System.out.printf("%n9x7=%d",a*7);
+        System.out.printf("%n9x8=%d",a*8);
+        System.out.printf("%n9x9=%d",a*9);
+        System.out.printf("%n9x10=%d",a*10);
+    }
+}
