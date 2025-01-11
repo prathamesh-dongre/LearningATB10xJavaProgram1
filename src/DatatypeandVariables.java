69a39543db2dc3fc304a8f24be4976627a2222df
Prathamesh <prathameshdongre0@gmail.com>
2025-01-11 14:24:55 +0530

    New change1

---
+++ src/DatatypeandVariables.java
@@ -0,0 +1,16 @@
+public class DatatypeandVariables {
+    public static void main(String[] args) {
+        int a =9 , b=20 ;
+        short v = 10 ;
+        float f= 10.4f;
+        double d= 31.8956883;
+        Boolean bull= true;
+        char c ='A';
+        System.out.println(a);
+        System.out.println(v);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.printf("my division is %d",a);
+
+    }
+}
