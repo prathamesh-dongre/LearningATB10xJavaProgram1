a528a185e5e55b44eed0354cde9413b3e3531bef
Prathamesh <prathameshdongre0@gmail.com>
2025-01-20 22:19:28 +0530

    New change1

--- src/Class002/EXPJAVA_003Inc_Dec_operator.java
+++ src/Class002/EXPJAVA_003Inc_Dec_operator.java
@@ -2,11 +2,15 @@ package Class002;
 
 public class EXPJAVA_003Inc_Dec_operator {
      public static void main (String[]args){
-         int a=20;
+         int a=10;
          System.out.println (++a + a++ + a++);
-           //line || 21+21+22= 64
+           //line || 11+11+12= 34
          System.out.println(a);
-         //a=23
-
+         //a=13
+         int b=20;
+         System.out.println(--b +b++ + b--);
+         System.out.println(b);
+         // line || 19+19+20= 58
+         //b=19
      }
 }
