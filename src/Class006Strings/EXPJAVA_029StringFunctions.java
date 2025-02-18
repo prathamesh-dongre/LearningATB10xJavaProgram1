99735578473ec3f9c838fc3929edb118a0b5cb4b
Prathamesh <prathameshdongre0@gmail.com>
2025-02-18 23:43:16 +0530

    New change1

--- src/Class006Strings/EXPJAVA_029StringFunctions.java
+++ src/Class006Strings/EXPJAVA_029StringFunctions.java
@@ -1,4 +1,29 @@
 package Class006Strings;
 
+import java.util.Locale;
+
 public class EXPJAVA_029StringFunctions {
+    public static void main(String[] args) {
+
+        String s1="Prathamesh";
+
+        System.out.println(s1.length());
+
+        System.out.println(s1.concat(" Dongre"));
+
+        System.out.println(s1.toUpperCase());
+
+        System.out.println(s1.toLowerCase());
+
+        System.out.println(s1.contains("rat"));
+
+        System.out.println(s1.equalsIgnoreCase("prathamesh"));
+
+        // Returns the index within this string of the
+        // first occurrence of the specified substring.
+
+        System.out.println(s1.indexOf("a"));
+
+
+    }
 }
