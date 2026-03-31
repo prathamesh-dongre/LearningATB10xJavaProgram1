c17e1ef7633fd7c80ad5ebf1d51fb86f566aa951
Prathamesh <prathameshdongre0@gmail.com>
2026-03-31 18:40:16 +0530

    New change1

---
+++ src/Class017_CollectionFrameworks/Map/EXP_112_Map_ArryList.java
@@ -0,0 +1,29 @@
+package Class017_CollectionFrameworks.Map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class EXP_112_Map_ArryList {
+    public static void main(String[] args) {
+
+        HashMap<String,String> student1 = new HashMap();
+        student1.put("name","pramod");
+        student1.put("roll","1");
+        student1.put("phone","96543210");
+        System.out.println(student1);
+
+        HashMap<String,String> student2 = new HashMap();
+        student2.put("name","amit");
+        student2.put("roll","2");
+        student2.put("phone","65432356");
+        System.out.println(student2);
+
+        List students = new ArrayList();
+        students.add(student1);
+        students.add(student2);
+        System.out.println(students);
+        students.remove(student2);
+        System.out.println(students);
+    }
+}
